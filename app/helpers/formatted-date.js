45ae13390d0a78a943898dd0364881ce28701f39
jelhan <jeldrik@systemli.org>
2015-04-09 17:45:00 +0200

    update formatted dates on locale change

--- app/helpers/formatted-date.js
+++ app/helpers/formatted-date.js
@@ -1,8 +1,14 @@
+import Ember from "ember";
 /* global moment */
 
 export default function(date, options) {
   var times = options.hash.times ? options.hash.times : false,
-      format = options.hash.format ? options.hash.format : 'LLLL';
+      format = options.hash.format ? options.hash.format : 'LLLL',
+      application = this.get('container').lookup('application:main'),
+      self = this;
+  
+  Ember.addObserver(application, 'locale', self, self.rerender);
+      
   if (times === true) {
     return moment(date).format( format );
   }
