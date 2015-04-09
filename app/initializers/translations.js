90900a5bac43ab492c8b07a66db8ab5ae94972c3
jelhan <jeldrik@systemli.org>
2015-04-09 17:59:26 +0200

    fix line indent

--- app/initializers/translations.js
+++ app/initializers/translations.js
@@ -4,52 +4,52 @@ import getLocales from "ember-cli-i18n-locales/utils/get-locales";
 /* global webshim */
 
 export default {
-    name: 'i18n',
-    initialize: function(container, application) {
-        var locale = '';
-        var availableLocales = getLocales();
-        var localeIsAvailable = function(locale) {
-          return availableLocales.indexOf(locale) !== -1;
-        };
-       
-        // get locale if it's already stored in a cookie
-        var cookie = document.cookie.replace(" ", "").split(";");
-        cookie.forEach(function(t){
-          var x = t.split("=");
-          if (x[0] === "locale") {
-            locale = x[1];
-          }
-        });
-      
-        // otherwise set locale by browser language
-        if (!locale) {
-          locale = (window.navigator.userLanguage || window.navigator.language).split("-")[0];
-        }
-
-        // use default locale if locale is not available
-        if(!localeIsAvailable(locale)) {
-          locale = application.get('defaultLocale');
-        }
-        
-        if(locale !== application.get('locale')) {
-          application.set('locale', locale);
-        }
-        // set locale of libaries used
-        moment.locale(locale);
-        webshim.activeLang(locale);
-
-        // update locale of libaries used if application locale changes
-        Ember.addObserver(application, 'locale', function() {
-          var locale = application.get('locale');
-
-          // update locale of libaries used if application locale changes
-          moment.locale(locale);
-          webshim.activeLang(locale);
-
-          // store or update selected locale in cookie
-          document.cookie="locale=" + locale + ";" +
-                          // give cookie a lifetime of one year
-                          "max-age=" + 60*60*24*356 + ";";
-        });
+  name: 'i18n',
+  initialize: function(container, application) {
+    var locale = '';
+    var availableLocales = getLocales();
+    var localeIsAvailable = function(locale) {
+      return availableLocales.indexOf(locale) !== -1;
+    };
+   
+    // get locale if it's already stored in a cookie
+    var cookie = document.cookie.replace(" ", "").split(";");
+    cookie.forEach(function(t){
+      var x = t.split("=");
+      if (x[0] === "locale") {
+        locale = x[1];
+      }
+    });
+  
+    // otherwise set locale by browser language
+    if (!locale) {
+      locale = (window.navigator.userLanguage || window.navigator.language).split("-")[0];
     }
+
+    // use default locale if locale is not available
+    if(!localeIsAvailable(locale)) {
+      locale = application.get('defaultLocale');
+    }
+    
+    if(locale !== application.get('locale')) {
+      application.set('locale', locale);
+    }
+    // set locale of libaries used
+    moment.locale(locale);
+    webshim.activeLang(locale);
+
+    // update locale of libaries used if application locale changes
+    Ember.addObserver(application, 'locale', function() {
+      var locale = application.get('locale');
+
+      // update locale of libaries used if application locale changes
+      moment.locale(locale);
+      webshim.activeLang(locale);
+
+      // store or update selected locale in cookie
+      document.cookie="locale=" + locale + ";" +
+                      // give cookie a lifetime of one year
+                      "max-age=" + 60*60*24*356 + ";";
+    });
+  }
 };
